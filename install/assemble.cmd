835b6478cd18ad46af13faf15078b759e2cc80bd
dlukyanov@ukr.net <dlukyanov@ukr.net@8bfaabc5-5968-4388-b6c2-9900562017e9>
2010-03-26 17:57:33 +0000

    package with version info
    git-svn-id: file:///c/Users/zrh535/projects/svn_pborca/trunk@7 8bfaabc5-5968-4388-b6c2-9900562017e9

--- install/assemble.cmd
+++ install/assemble.cmd
@@ -1,8 +1,15 @@
 @echo off
-del /Q pborcaw.zip
-svn export deploy pborcaw
-svn export ..\test2 pborcaw\test2
-copy ..\bin\*.exe pborcaw\
-pkzipc -add -lev=9 -path=specify -rec pborcaw.zip pborcaw\*.*
-rmdir /Q /S pborcaw
-copy /Y deploy\pborca.htm .\
+del /Q *.zip
+
+if NOT EXIST ..\bin\pborca.exe exit /0
+
+for /F %%i in ('..\bin\pborca.exe -v') do set PBORCA_%%i
+
+set target=pborca_%PBORCA_VERSION%
+
+svn export deploy %target%
+svn export ..\test2 %target%\test2
+
+copy ..\bin\*.exe %target%\
+pkzipc -add -lev=9 -path=specify -rec %target%.zip %target%\*.*
+rmdir /Q /S %target%
